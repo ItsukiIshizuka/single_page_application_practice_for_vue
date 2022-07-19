8f810cc9a5bee9e54bae0f613c56b33140d648a6
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2022-07-19 10:51:31 +0900

    vue-routerの設定確認のためpage1コンポーネント追加&page1componentをルーティングに設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,22 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
+
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import page1 from '@/components/page1'
 
 Vue.use(Router)
 
@@ -8,8 +24,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: page1
     }
   ]
 })
